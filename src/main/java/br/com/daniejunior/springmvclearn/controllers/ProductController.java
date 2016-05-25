28896794cfbf1fcbc1ecd9b4562d80f4c764d70a
Daniel <danieljunior@id.uff.br>
2016-05-25 13:58:33 -0300

    Adding product page view, with link at products list page

--- src/main/java/br/com/daniejunior/springmvclearn/controllers/ProductController.java
+++ src/main/java/br/com/daniejunior/springmvclearn/controllers/ProductController.java
@@ -9,6 +9,7 @@ import br.com.daniejunior.springmvclearn.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -29,5 +30,11 @@ public class ProductController {
     public String listProducts(Model model) {
         model.addAttribute("products", productService.listAllProducts());
         return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product";
     }
 }
--- src/main/java/br/com/daniejunior/springmvclearn/services/ProductService.java
+++ src/main/java/br/com/daniejunior/springmvclearn/services/ProductService.java
@@ -14,4 +14,5 @@ import java.util.List;
  */
 public interface ProductService {
     List<Product> listAllProducts();
+    Product getProductById(Integer id);
 }
