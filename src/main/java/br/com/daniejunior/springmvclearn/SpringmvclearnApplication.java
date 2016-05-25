b1f8e13331814db9656917fa12188b8a38be14ca
Daniel <danieljunior@id.uff.br>
2016-05-25 12:03:01 -0300

    Initializing to configure index controller and including jquery and bootstrap webjars

---
+++ src/main/java/br/com/daniejunior/springmvclearn/SpringmvclearnApplication.java
@@ -0,0 +1,12 @@
+package br.com.daniejunior.springmvclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvclearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvclearnApplication.class, args);
+	}
+}
